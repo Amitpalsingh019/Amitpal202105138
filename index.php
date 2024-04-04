d1b391e4a3067150fe7ca960dc300ce583c838b2
Amitpalsingh019 <159388962+Amitpalsingh019@users.noreply.github.com>
2024-04-04 00:51:04 -0400

    index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Dashboard</title>
+    <title>Grocery Dashboard</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
@@ -18,12 +18,23 @@
         table tr td:last-child {
             width: 120px;
         }
+        body{
+            background-color: #03d893;
+        }
     </style>
     <script>
         $(document).ready(function() {
             $('[data-toggle="tooltip"]').tooltip();
         });
     </script>
+    <div class="nav">
+        <a href="apple.html">HOME</a>
+        <a href="apple2.html">Weekend Sale</a>
+        <a href="apple3.html" class="active">Monthly Benefits</a>
+        <a href="apple4.html">Buy One Get One</a>
+        <a href="apple5.html">Careers</a>
+        <a href="index.php">add product</a>
+    </div>
 </head>
 
 <body>
@@ -32,38 +43,36 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="mt-5 mb-3 clearfix">
-                        <h2 class="pull-left">Employees Details</h2>
-                        <a href="create.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i> Add New Employee</a>
+                        <h2 class="pull-left">Grocery Details</h2>
+                        <a href="create_grocery.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i> Add New Item</a>
                     </div>
                     <?php
                     // Include config file
                     require_once "config.php";
 
                     // Attempt select query execution
-                    $sql = "SELECT * FROM employees";
+                    $sql = "SELECT product, price, quantity FROM grocery";
                     if ($result = mysqli_query($link, $sql)) {
                         if (mysqli_num_rows($result) > 0) {
                             echo '<table class="table table-bordered table-striped">';
                             echo "<thead>";
                             echo "<tr>";
-                            echo "<th>#</th>";
-                            echo "<th>Name</th>";
-                            echo "<th>location</th>";
-                            echo "<th>productssold</th>";
+                            echo "<th>Product</th>";
+                            echo "<th>Price</th>";
+                            echo "<th>Quantity</th>";
                             echo "<th>Action</th>";
                             echo "</tr>";
                             echo "</thead>";
                             echo "<tbody>";
                             while ($row = mysqli_fetch_array($result)) {
                                 echo "<tr>";
-                                echo "<td>" . $row['id'] . "</td>";
-                                echo "<td>" . $row['name'] . "</td>";
-                                echo "<td>" . $row['location'] . "</td>";
-                                echo "<td>" . $row['productssold'] . "</td>";
+                                echo "<td>" . $row['product'] . "</td>";
+                                echo "<td>" . $row['price'] . "</td>";
+                                echo "<td>" . $row['quantity'] . "</td>";
                                 echo "<td>";
-                                echo '<a href="read.php?id=' . $row['id'] . '" class="mr-3" title="View Record" data-toggle="tooltip"><span class="fa fa-eye"></span></a>';
-                                echo '<a href="update.php?id=' . $row['id'] . '" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>';
-                                echo '<a href="delete.php?id=' . $row['id'] . '" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash"></span></a>';
+                                echo '<a href="read_grocery.php?id=' . $row['id'] . '" class="mr-3" title="View Record" data-toggle="tooltip"><span class="fa fa-eye"></span></a>';
+                                echo '<a href="update_grocery.php?id=' . $row['id'] . '" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>';
+                                echo '<a href="delete_grocery.php?id=' . $row['id'] . '" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash"></span></a>';
                                 echo "</td>";
                                 echo "</tr>";
                             }
